f8259cb2e648f77286014c85fd426a77bc823fdb
Audhip Chowdhury <audhipchowdhury@gmail.com>
2026-05-20 02:32:07 +0530

    delpoy_edits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ["seekjob.audhip-projects.com"],
+  },
+  preview: {
+    host: true,
+    allowedHosts: ["seekjob.audhip-projects.com"],
+  },
 });
